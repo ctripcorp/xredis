d5f28236b2e277796af505f4ab2b2ec15b6943f7
phyxdown <phyxdown@qq.com>
2022-02-09 03:09:58 +0800

    method definition of gtid_set

---
+++ src/ctrip_gtid.c
@@ -0,0 +1,16 @@
+#include "server.h"
+
+char* encode(gtid_set *gtid_set) {
+
+    return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA:1-7";
+}
+
+gtid_set* decode(char* gtid) {
+
+    return NULL;
+}
+
+gtid_set* add_gtid(gtid_set* gtid_set, char* gtid) {
+
+    return NULL;
+}
--- src/ctrip_gtid.h
+++ src/ctrip_gtid.h
@@ -4,6 +4,8 @@
 #include "server.h"
 
 typedef struct gtid_interval gtid_interval;
+typedef struct uuid_set uuid_set;
+typedef struct gtid_set gtid_set;
 
 typedef long long int rpl_gno;
 
@@ -25,5 +27,11 @@ struct gtid_set {
 
     uuid_set* uuid_sets;
 };
+
+char *encode(gtid_set*);
+
+gtid_set* decode(char*);
+
+gtid_set* add_gtid(gtid_set*, char*);
 
 #endif  /* __REDIS_CTRIP_GTID_H */
