eacbb184dbe77e78b123591b6193dfeab0cf9785
Victor <weicheng.king@qq.com>
2024-05-17 16:04:40 +0800

    bitmap add ror tcl imp56

--- tests/test_helper.tcl
+++ tests/test_helper.tcl
@@ -14,6 +14,7 @@ source tests/support/ctrip_util.tcl
 source tests/swap/support/util.tcl
 
 set ::disk_tests {
+    swap/integration/rordb
     swap/integration/client_rate_limit_bug
     swap/integration/type_error
     swap/integration/zset
