b6b75256d674bf28d07713aaf72f39705b35a50d
Victor <weicheng.king@qq.com>
2024-12-17 13:27:01 +0000

    [fix] fix ttl compact after flushdb

--- tests/swap/unit/ttl_compact.tcl
+++ tests/swap/unit/ttl_compact.tcl
@@ -1,4 +1,4 @@
-start_server {tags {"ttl compact 0"} 
+start_server {tags {"ttl-compact"} 
     overrides {swap-debug-evict-keys {0} 
                swap-ttl-compact-period {1} 
                swap-sst-age-limit-refresh-period {1} 
@@ -28,7 +28,7 @@ start_server {tags {"ttl compact 0"}
     }
 }
 
-start_server {tags {"ttl compact 1"} 
+start_server {tags {"ttl-compact"} 
     overrides {swap-debug-evict-keys {0} 
                swap-ttl-compact-period {1} 
                swap-sst-age-limit-refresh-period {1} 
@@ -67,7 +67,7 @@ start_server {tags {"ttl compact 1"}
     }
 }
 
-start_server {tags {"ttl compact 2"} 
+start_server {tags {"ttl-compact"} 
     overrides {swap-debug-evict-keys {0} 
                swap-ttl-compact-period {1} 
                swap-sst-age-limit-refresh-period {1} 
@@ -87,7 +87,7 @@ start_server {tags {"ttl compact 2"}
         # sst in L0 is forced to be compacted to L1
         r swap compact 
 
-        # make sure info property updated
+        # more than swap-sst-age-limit-refresh-period
         after 1200
 
         set request_sst_count [get_info_property r Swap swap_ttl_compact request_sst_count]
@@ -107,7 +107,7 @@ start_server {tags {"ttl compact 2"}
     }
 }
 
-start_server {tags {"ttl compact 3"} 
+start_server {tags {"ttl-compact"} 
     overrides {swap-debug-evict-keys {0} 
                swap-ttl-compact-period {1} 
                swap-sst-age-limit-refresh-period {1} 
@@ -128,7 +128,7 @@ start_server {tags {"ttl compact 3"}
         # sst in L0 is forced to be compacted to L1
         r swap compact 
 
-        # 1.2s, bigger than swap-ttl-compact-period
+        # 1.2s, more than swap-ttl-compact-period
         after 1200
 
         set sst_age_limit [get_info_property r Swap swap_ttl_compact sst_age_limit]
@@ -160,8 +160,8 @@ start_server {tags {"ttl compact 3"}
         # sst in L0 is forced to be compacted to L1
         r swap compact 
 
-        # 1.6s, much bigger than ttl compact period
-        after 1600
+        # 1.2s, much bigger than ttl compact period
+        after 1200
 
         set sst_age_limit [get_info_property r Swap swap_ttl_compact sst_age_limit]
         assert_range $sst_age_limit 500 1000
@@ -180,7 +180,7 @@ start_server {tags {"ttl compact 3"}
     }
 }
 
-start_server {tags {"ttl compact 4"} 
+start_server {tags {"ttl-compact"} 
     overrides {swap-debug-evict-keys {0} 
                swap-ttl-compact-period {1} 
                swap-sst-age-limit-refresh-period {1} 
@@ -192,6 +192,7 @@ start_server {tags {"ttl compact 4"}
 
             # bitmap is spilt as subkey of 4KB by default
             r setbit $mybitmap 32768 1
+            r pexpire $mybitmap 1000
 
             r swap.evict $mybitmap
             wait_key_cold r $mybitmap
@@ -201,20 +202,17 @@ start_server {tags {"ttl compact 4"}
         after 1200
 
         set sst_age_limit [get_info_property r Swap swap_ttl_compact sst_age_limit]
-        assert_morethan $sst_age_limit 9000000000000000000
+        assert_range $sst_age_limit 800 1000
 
-        # sst_age_limit will be gradually 0 after flushdb if no new keys, just mock that  
-        # stop sst-age-limit refresh
-        r config set swap-sst-age-limit-refresh-period 3600
-
+        # sst_age_limit will be reset after flushdb
         r flushdb
 
         set sst_age_limit [get_info_property r Swap swap_ttl_compact sst_age_limit]
-        assert_equal $sst_age_limit 0
+        assert_morethan $sst_age_limit 9000000000000000000
     }
 }
 
-start_server {tags {"ttl compact 5"} 
+start_server {tags {"ttl-compact"} 
     overrides {swap-debug-evict-keys {0} 
                swap-ttl-compact-period {1} 
                swap-sst-age-limit-refresh-period {1} 
@@ -262,12 +260,11 @@ start_server {tags {"ttl compact 5"}
     }
 }
 
-start_server {tags {"master propagate expire test"} overrides {save ""}} {
+start_server {tags {"ttl-compact master propagate"} overrides {save ""}} {
 
     start_server {overrides {swap-repl-rordb-sync {no} 
                              swap-debug-evict-keys {0}
-                             swap-swap-info-slave-period {1} 
-                             swap-sst-age-limit-refresh-period {1000}}} {
+                             swap-ttl-compact-enabled {no} }} {
 
         set master_host [srv 0 host]
         set master_port [srv 0 port]
@@ -279,16 +276,41 @@ start_server {tags {"master propagate expire test"} overrides {save ""}} {
         wait_for_sync $slave
         test {ttl compact master slave propagate check} {
 
+            # ttl-compact closed, sst_age_limit is initial value
+            set sst_age_limit1 [get_info_property $master Swap swap_ttl_compact sst_age_limit]
+            assert_morethan $sst_age_limit1 9000000000000000000
+
+            # sst_age_limit in slave is also initial value
+            set sst_age_limit2 [get_info_property $slave Swap swap_ttl_compact sst_age_limit]
+            assert_equal $sst_age_limit1 $sst_age_limit2
+
+            # master open ttl compact, but not propagate sst_age_limit to slave right now
+            $master config set swap-swap-info-slave-period 36000
+            $master config set swap-ttl-compact-enabled yes
+            $master config set swap-sst-age-limit-refresh-period 1
+
+            after 1200
+
+            # sst_age_limit in master is set to 0 for the empty dataset
+            set sst_age_limit1 [get_info_property $master Swap swap_ttl_compact sst_age_limit]
+            assert_equal $sst_age_limit1 0
+
+            # sst_age_limit in slave is still initial value
+            set sst_age_limit2 [get_info_property $slave Swap swap_ttl_compact sst_age_limit]
+            assert_morethan $sst_age_limit2 9000000000000000000
+
+            # master propagate sst_age_limit to slave, and stop refresh its own
+            $master config set swap-swap-info-slave-period 1
+            $master config set swap-sst-age-limit-refresh-period 36000
+
             $master swap.info SST-AGE-LIMIT 1111
 
-            # more than swap-swap-info-slave-period
-            after 1100
-            # wait_for_ofs_sync $master $slave
-
             set sst_age_limit1 [get_info_property $master Swap swap_ttl_compact sst_age_limit]
-            set sst_age_limit2 [get_info_property $slave Swap swap_ttl_compact sst_age_limit]
-
             assert_equal $sst_age_limit1 1111
+
+            after 1200
+
+            set sst_age_limit2 [get_info_property $slave Swap swap_ttl_compact sst_age_limit]
             assert_equal $sst_age_limit1 $sst_age_limit2
         }
     }
